c50a63365bf4c9ab379e3390fe287435207a99d0
chaou1 <77403427+chaou1@users.noreply.github.com>
2022-05-14 19:07:48 +0200

    a

--- game/Assets/Scripts/BasicMovement.cs
+++ game/Assets/Scripts/BasicMovement.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public int a= 0;
+    public Transform playerposition;
+ 
     // Start is called before the first frame update
     void start() { }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W) )
+        {
+            playerposition.position += new Vector3(0, 0.01f ,0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerposition.position += new Vector3(-0.01f, 0,0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerposition.position += new Vector3(0, -0.01f, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerposition.position += new Vector3(0.01f, 0,0);
+        }
     }
+    
     
 }
