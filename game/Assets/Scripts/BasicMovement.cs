6b526c70c5e7ba49f1ee52da9e97327b3daf92ec
chaou1 <77403427+chaou1@users.noreply.github.com>
2022-05-14 18:38:20 +0200

    a

---
+++ game/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public int a= 0;
+    // Start is called before the first frame update
+    void start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
