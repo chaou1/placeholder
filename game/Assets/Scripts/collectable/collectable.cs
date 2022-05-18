b5bc9de880ca7dcbc2e1ead1ff4ea3f761f2714e
Dredith <95763867+Dredith@users.noreply.github.com>
2022-05-18 21:40:12 +0200

    Konflikt baby

--- game/Assets/Scripts/collectable/collectable.cs
+++ game/Assets/Scripts/collectable/collectable.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class collectable : MonoBehaviour
-{
+{   
     public Transform collectablepostition;
+    public Transform playerposition;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +15,28 @@ public class collectable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+        if(playerposition.position.x < collectablepostition.position.x) {
+            print("KONFLIKT!!!!!");
+        }
+        */
+        collectablepostition.position += new Vector3(0.005f, 0, 0);
+      
+       
 
-        collectablepostition.position += new Vector3(0, 0.1f, 0);
-        collectablepostition.position += new Vector3(0, -0.1f, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player" )) {
+            print("KONFLIKT!!!!!");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            print("KONFLIKT!!!!!");
+        }
 
     }
 
