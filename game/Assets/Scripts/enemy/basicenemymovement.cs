f2696db9898d3f29174dbfb385ca2e29996e4122
Ruihaolol <ruihao111@gmail.com>
2022-05-18 21:04:45 +0200

    enemym

---
+++ game/Assets/Scripts/enemy/basicenemymovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Transform playerposition;
+    public Transform enemyposition;
+    public Vector2 playerpositionv = new Vector2();
+    public Vector2 enemypositionv = new Vector2();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attackmovement();
+    }
+    public void Attackmovement() {
+        playerpositionv = playerposition.transform.position;
+        enemypositionv = enemyposition.transform.position;
+
+
+    }
+   
+}
