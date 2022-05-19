3741d1ea475ef8ab9b1b3f0f816ad22b2116b900
chaou1 <77403427+chaou1@users.noreply.github.com>
2022-05-19 16:30:56 +0200

    a

--- game/Assets/Scripts/projectile/projectile.cs
+++ game/Assets/Scripts/projectile/projectile.cs
@@ -7,6 +7,7 @@ public class projectile : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     public Vector2 currentdistance;
+    public bool test= false;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +18,14 @@ public class projectile : MonoBehaviour
     void Update()
     {
         rb.MovePosition(rb.position + currentdistance * speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      
+        if (collision.CompareTag("enemy")) 
+        {
+            test = true;
+            Destroy(collision);
+        }
     }
 }
