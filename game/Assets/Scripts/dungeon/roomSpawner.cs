fa23d8e7c1c43d5b547c68b8b4630b48f0915803
chaou1 <77403427+chaou1@users.noreply.github.com>
2022-05-20 17:01:20 +0200

    f

--- game/Assets/Scripts/dungeon/roomSpawner.cs
+++ game/Assets/Scripts/dungeon/roomSpawner.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class roomSpawner : MonoBehaviour
 {
     public int openingDirection;
+    public roomTemplates roomtemplate;
     // Start is called before the first frame update
     void Start()
     {
-        
+        roomtemplate = GameObject.FindGameObjectWithTag("Rooms").GetComponent<roomTemplates>();
     }
 
     // Update is called once per frame
---
+++ game/Assets/Scripts/dungeon/roomTemplates.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomTemplates : MonoBehaviour
+{
+    public GameObject[] roomT;
+    public GameObject[] roomR;
+    public GameObject[] roomD;
+    public GameObject[] roomL;
+
+}
