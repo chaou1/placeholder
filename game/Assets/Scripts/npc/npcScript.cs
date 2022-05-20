5af2628d00a1c80bdd063eb71bd88b922ad4e6e1
Ruihaolol <ruihao111@gmail.com>
2022-05-20 17:58:35 +0200

    dddd

--- game/Assets/Scripts/npc/npcScript.cs
+++ game/Assets/Scripts/npc/npcScript.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class npcScript : dialogsystem
 {
-
+    public string[] dialogs;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        DialogBox.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dialog();
     }
-    
+    protected override void Text()
+    {
+        base.Text();
+    }
 }
