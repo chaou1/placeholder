3d426b1d2acc08ee6a23d0b5ec813da5e36468a3
Dredith <95763867+Dredith@users.noreply.github.com>
2022-05-21 17:52:12 +0200

    lk

--- game/Assets/Scripts/player/PlayerInteraction.cs
+++ game/Assets/Scripts/player/PlayerInteraction.cs
@@ -26,38 +26,19 @@ public class PlayerInteraction : MonoBehaviour
             Interactable interactable = hit.collider.GetComponent<Interactable>();
             if (interactable != null)
             {
-               // SetFocus(interactable);
+                RemoveFocus();
                 Debug.Log("Irgenwasddas: " + hit.collider.gameObject.transform);
             }
-        }
-        /*
-        if (Input.GetMouseButtonDown(1))
-        {Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if(Physics.Raycast(ray, out hit))
-            {Debug.Log("tagsasga");}}
-            /*
-        if (Input.GetMouseButtonDown(1) ) {RaycastHit2D rayHit = Physics2D.GetRayIntersection(cam.ScreenPointToRay(Input.mousePosition));
-                {Interactable interactable = rayHit.transform.GetComponent<Interactable>();
-                    if (interactable != null{RemoveFocus();}
-                    //Debug.Log(rayHit.transform.tag);}}
-            */
-        if (Input.GetMouseButtonDown(0)) {
+        }    
+        if (Input.GetMouseButtonDown(0)) 
+        {
             RaycastHit2D rayHit = Physics2D.GetRayIntersection(cam.ScreenPointToRay(Input.mousePosition));
-
-          //  if(Physics2D.Raycast(Ray2D, out rayHit, 100))
-            if (rayHit.transform.CompareTag("Collectable"))
-            {
-                
-               Interactable interactable = rayHit.collider.GetComponent<Interactable>();
+                Interactable interactable = rayHit.collider.GetComponent<Interactable>();
                 if (rayHit != null)
                 {
                     Debug.Log( "Fokus");
-                    //SetFocus(interactable);
+                    SetFocus(interactable);
                 }
-                
-               
-            }
         }
     }
 
@@ -67,8 +48,7 @@ public class PlayerInteraction : MonoBehaviour
     {
         var item = other.GetComponent<Item>();
         if (item)  //other.GetComponent<Item>() auch möglich  other.CompareTag("Collectable")
-        {
-          
+        {         
             inventory.AddItem(item.item, 1);
             Debug.Log(item.name);
             Destroy(other.gameObject);
@@ -80,16 +60,16 @@ public class PlayerInteraction : MonoBehaviour
     }
     void SetFocus(Interactable newFocus)
     {
-      //  if (newFocus != focus)
-       // {
-        //    focus.DeFocused();
-       // }
+        if (newFocus != focus) //
+        {
+           focus.DeFocused(); //
+        } 
         focus = newFocus;
-        //newFocus.OnFocused(transform);
+        newFocus.OnFocused(transform); //
     }
     void RemoveFocus()
     {
-        //focus.DeFocused();
+        focus.DeFocused(); //
         focus = null;
     }
 }
